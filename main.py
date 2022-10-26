e41c315bbd5acdd798f4effad1785cf5160edd6f
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2022-10-26 17:03:51 -0300

    Atualiza o código para o segundo turno
    - Muda o link do JSON
    - Cria nova coluna para mostrar a diferença dos votos
    - Cria estrutura de comparação do número de votos

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import requests
 import json
 import pandas as pd
 
-data = requests.get(
-    'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
+data = requests.get('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json')
 
 json_data = json.loads(data.content)
 
@@ -11,16 +10,24 @@ candidato = []
 partido = []
 votos = []
 porcentagem = []
+diferenca = []
 
 for informacoes in json_data['cand']:
     
-    if informacoes['seq'] in ['1', '2', '3', '4']:
+    if informacoes['seq'] in ['1', '2']:
         candidato.append(informacoes['nm'])
         votos.append(informacoes['vap'])
-        porcentagem.append(informacoes['pvap'])
-        
-df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns = [
-    'Candidato', 'Nº de Votos', 'Porcentagem'
+        porcentagem.append(informacoes['pvap'] + '%')
+
+        if informacoes['seq'] == '1':
+            diferenca.append('1º lugar')
+
+        elif informacoes['seq'] == '2':
+            diferenca.append((int(json_data['cand'][0]['vap']) - int(informacoes['vap'])))
+
+
+df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem, diferenca)), columns = [
+    'Candidato | ', 'Nº de Votos | ', 'Porcentagem | ', 'Votos para 1º colocado'
 ])
 
 print(df_eleicao)
